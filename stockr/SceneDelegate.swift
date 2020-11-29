1a63ea0e340303ff1d174ead0b638aacd31fdf7f
Joschua Marz <joschua.marz@gmx.de>
2020-11-29 09:57:32 +0100

    transparent tabbar

--- stockr/SceneDelegate.swift
+++ stockr/SceneDelegate.swift
@@ -14,7 +14,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        
+        //Dark Mode
         window?.overrideUserInterfaceStyle = .dark
+        
+        //Transparente Tabbar
+        
+        if let tabBarController = window?.rootViewController as? UITabBarController {
+            tabBarController.tabBar.backgroundImage = UIImage()
+            tabBarController.tabBar.shadowImage = UIImage()
+            tabBarController.tabBar.clipsToBounds = true
+        }
+        
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
